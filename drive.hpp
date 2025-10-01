59a52c1c74684aa37ca72dec21ed44d5527694da
Steiynbrodt <180658853+Steiynbrodt@users.noreply.github.com>
2025-10-01 15:45:44 +0200

    Add files via upload

--- drive.hpp
+++ drive.hpp
@@ -14,25 +14,28 @@ controller Controller1 = controller(primary);
 gps GPS17 = gps(PORT15,-100.00, +30.00, mm, +90);
 
 inertial INS  = inertial(PORT14);
-motor driveMotorLeftOne = motor(PORT21, ratio18_1, false);  
-motor driveMotorLeftTwo = motor(PORT12, ratio18_1, false); 
-motor driveMotorLeftThree = motor(PORT13, ratio18_1, false);
+
+motor driveMotorLeftOne = motor(PORT6, ratio18_1, false);  
+motor driveMotorLeftTwo = motor(PORT1, ratio18_1, false); 
+motor driveMotorLeftThree = motor(PORT4, ratio18_1, false);
 
 
-motor driveMotorRightOne = motor(PORT18, ratio18_1, true); 
+motor driveMotorRightOne = motor(PORT8, ratio18_1, true); 
 motor driveMotorRightTwo = motor(PORT9, ratio18_1, true);
-motor driveMotorRightThree = motor(PORT11, ratio18_1, true);  
+motor driveMotorRightThree = motor(PORT10, ratio18_1, true);  
 
+motor in1 = motor(PORT20, ratio18_1, false);  
+motor in2 = motor(PORT11 ,ratio18_1, false); 
+motor in3 = motor(PORT5, ratio18_1, false); 
+//motor in4 = motor(PORT5, ratio18_1, false); 
 
 motor_group RightDrivetrain = motor_group(driveMotorRightOne, driveMotorRightTwo, driveMotorRightThree);
 motor_group LeftDrivetrain  = motor_group(driveMotorLeftOne, driveMotorLeftTwo, driveMotorLeftThree);
 motor_group FullDrivetrain = motor_group(driveMotorRightOne, driveMotorRightTwo, driveMotorRightThree, driveMotorLeftOne, driveMotorLeftTwo, driveMotorLeftThree);
 bool DrivetrainNeedsToBeStopped_Controller1 = false;
 
-motor in1 = motor(PORT1, ratio18_1, false);  
-motor in2 = motor(PORT4 ,ratio18_1, false); 
-motor in3 = motor(PORT3, ratio18_1, false); 
-motor in4 = motor(PORT5, ratio18_1, false); 
+
+
 #pragma endregion VEXcode Generated Robot Configuration
 
 
@@ -48,7 +51,7 @@ void inntake(int intake ){
 }
 
 
-  // --- Button handlers (no blocking, no while-loops) ---
+ //buttons 
 void R1Pressed()  { in1.setVelocity(100, percent); in1.spin(forward); }
 void R1Released() { in1.stop(coast); }
 
@@ -70,36 +73,42 @@ void yPressed()  { in2.setVelocity(100, percent); in2.spin(reverse); }
 void yReleased() { in2.stop(coast); }
 
 
-// --- Driver control using real callbacks ---
+
 void drivercontrol(void) {
-  // Bind callbacks ONCE (outside the loop)
   
+  // --- Button bindings (pressed + released) ---
   Controller1.ButtonR1.pressed(R1Pressed);
-  
+  Controller1.ButtonR1.released(R1Released);
+
   Controller1.ButtonR2.pressed(R2Pressed);
-  
+  Controller1.ButtonR2.released(R2Released);
 
   Controller1.ButtonL1.pressed(L1Pressed);
-  
-  Controller1.ButtonL2.pressed(L2Pressed);
-  
+  Controller1.ButtonL1.released(L1Released);
 
+  Controller1.ButtonL2.pressed(L2Pressed);
+  Controller1.ButtonL2.released(L2Released);
+
+  // If you want Right + Y to control in2 (as in your code)
   Controller1.ButtonRight.pressed(RrPressed);
-  
+  Controller1.ButtonRight.released(RrReleased);
 
   Controller1.ButtonY.pressed(yPressed);
-  
+  Controller1.ButtonY.released(yReleased);
+
+  // If you actually wanted Up (instead of Right), use this instead:
+  // Controller1.ButtonUp.pressed(RrPressed);
+  // Controller1.ButtonUp.released(RrReleased);
 
 while (true) {
   
-    // Arcade drive (same math you had)
     double left  = Controller1.Axis3.position() - Controller1.Axis1.position() / 2.0;
     double right = Controller1.Axis3.position() + Controller1.Axis1.position() / 2.0;
 
     LeftDrivetrain.spin(reverse, left, percent);
     RightDrivetrain.spin(reverse, right, percent);
 
-    wait(20, msec); // let the scheduler run the callbacks
+    wait(20, msec); 
     
   
 }
